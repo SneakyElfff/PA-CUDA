e975ab3e468f721ee837861c164465471300fdc2
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2024-09-28 12:12:39 +0300

    version for different cache layers added

--- LP1/main.cpp
+++ LP1/main.cpp
@@ -6,7 +6,9 @@
 
 using namespace std;
 
-constexpr int BLOCK_SIZE = 576;
+const int BLOCK_SIZE_L1 = 222;
+const int BLOCK_SIZE_L2 = 576;
+// const int BLOCK_SIZE_L3 = ;
 
 vector<vector<int> > fillMatrix(int rows, int cols) {
     vector<vector<int> > matrix(rows, vector<int>(cols));
@@ -24,7 +26,7 @@ vector<vector<int> > fillMatrix(int rows, int cols) {
     return matrix;
 }
 
-vector<vector<int> > multiplyMatrices(const vector<vector<int> >& matrix1, const vector<vector<int>>&matrix2, int M, int N, int K) {
+vector<vector<int> > multiplyMatrices(const vector<vector<int> >& matrix1, const vector<vector<int> >& matrix2, int M, int N, int K) {
     vector<vector<int> > result(M, vector<int>(K, 0));
 
     for (int i = 0; i < M; i++) {
@@ -38,15 +40,15 @@ vector<vector<int> > multiplyMatrices(const vector<vector<int> >& matrix1, const
     return result;
 }
 
-vector<vector<int> > blockMultiplyMatrices(const vector<vector<int> >& matrix1, const vector<vector<int> >&matrix2, int M, int N, int K) {
+vector<vector<int> > blockMultiplyMatrices(const vector<vector<int> >& matrix1, const vector<vector<int> >& matrix2, int M, int N, int K, int block_size) {
     vector<vector<int> > result(M, vector<int>(K, 0));
 
-    for (int i = 0; i < M; i += BLOCK_SIZE) {
-        for (int j = 0; j < K; j += BLOCK_SIZE) {
-            for (int k = 0; k < N; k += BLOCK_SIZE) {
-                int i_m = min(i + BLOCK_SIZE, M);
-                int j_m = min(j + BLOCK_SIZE, K);
-                int k_m = min(k + BLOCK_SIZE, N);
+    for (int i = 0; i < M; i += block_size) {
+        for (int j = 0; j < K; j += block_size) {
+            for (int k = 0; k < N; k += block_size) {
+                int i_m = min(i + block_size, M);
+                int j_m = min(j + block_size, K);
+                int k_m = min(k + block_size, N);
 
                 // перемножение блоков
                 for (int ib = i; ib < i_m; ++ib) {
@@ -80,9 +82,8 @@ bool compareMatrices(const vector<vector<int> >& matrix1, const vector<vector<in
 
 int main()
 {
-    // каждая матрица > 16 Мб
-    int M = 4096;
-    int N = 16384;
+    int M = 2048;
+    int N = 8000;
     int K = 1024;
 
     auto matrix1 = fillMatrix(M, N);
@@ -93,24 +94,35 @@ int main()
     auto start = chrono::high_resolution_clock::now();
     auto result = multiplyMatrices(matrix1, matrix2, M, N, K);
     auto end = chrono::high_resolution_clock::now();
-
     chrono::duration<double> duration = end - start;
-    cout << "Перемножение матриц классическим способом завершено." << endl;
     cout << "Длительность классического перемножения: " << duration.count() << " секунд." << endl << endl;
 
     cout << "Перемножение матриц блочным способом начато." << endl;
 
     start = chrono::high_resolution_clock::now();
-    auto result_blocked = blockMultiplyMatrices(matrix1, matrix2, M, N, K);
+    auto result_blocked_L1 = blockMultiplyMatrices(matrix1, matrix2, M, N, K, BLOCK_SIZE_L1);
     end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration_blocked_L1 = end - start;
+    cout << "Длительность блочного перемножения для L1 кэша: " << duration_blocked_L1.count() << " секунд." << endl;
 
-    chrono::duration<double> duration_blocked = end - start;
-    cout << "Перемножение матриц блочным способом завершено." << endl;
-    cout << "Длительность блочного перемножения: " << duration_blocked.count() << " секунд." << endl;
+    start = chrono::high_resolution_clock::now();
+    auto result_blocked_L2 = blockMultiplyMatrices(matrix1, matrix2, M, N, K, BLOCK_SIZE_L2);
+    end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration_blocked_L2 = end - start;
+    cout << "Длительность блочного перемножения для L2 кэша: " << duration_blocked_L2.count() << " секунд." << endl;
 
-    cout << endl << "Блочная реализация быстрее, чем классическая в " << duration.count() / duration_blocked.count() << " раз." << endl;
+    // start = chrono::high_resolution_clock::now();
+    // auto result_blocked_L3 = blockMultiplyMatrices(matrix1, matrix2, M, N, K, BLOCK_SIZE_L3);
+    // end = chrono::high_resolution_clock::now();
+    // chrono::duration<double> duration_blocked_L3 = end - start;
+    // cout << "Длительность блочного перемножения для L3 кэша: " << duration_blocked_L3.count() << " секунд." << endl;
 
-    if (compareMatrices(result, result_blocked)) {
+    cout << endl << "Блочная реализация L1 быстрее, чем классическая в " << duration.count() / duration_blocked_L1.count() << " раз." << endl;
+    cout << "Блочная реализация L2 быстрее, чем классическая в " << duration.count() / duration_blocked_L2.count() << " раз." << endl;
+    // cout << "Блочная реализация L3 быстрее, чем классическая в " << duration.count() / duration_blocked_L3.count() << " раз." << endl;
+
+    if (compareMatrices(result, result_blocked_L1) && compareMatrices(result, result_blocked_L2)) {
+    // if (compareMatrices(result, result_blocked_L1) && compareMatrices(result, result_blocked_L2) && compareMatrices(result, result_blocked_L3)) {
         cout << "Результаты реализаций совпадают." << endl;
         cout << "Первый элемент матрицы: " << result[0][0] << "." << endl;
     }
@@ -118,12 +130,17 @@ int main()
         cout << "Результаты реализаций НЕ совпадают." << endl;
 
     cout << endl << "Результаты тестирования:" << endl;
-    cout << left << setw(10) << "M" << setw(10) << "N" << setw(10) << "N" << setw(10) << "K"
-         << setw(20) << "Время-1 (с)" << setw(20) << "Время-2 (с)" << endl;
-    cout << string(74, '-') << endl;
+    cout << left << setw(8) << "M" << setw(8) << "N" << setw(8) << "K"
+         << setw(20) << "Время-1 (с)" << setw(20) << "L1 (с)"
+         << setw(20) << "L2 (с)" << setw(20) << "L3 (с)" << endl;
+    cout << string(84, '-') << endl;
 
-    cout << left << setw(10) << M << setw(10) << N << setw(10) << N << setw(10) << K
-         << setw(20) << duration.count() << duration_blocked.count() << endl;
+    cout << left << setw(8) << M << setw(8) << N << setw(8) << K
+         << setw(14) << duration.count()
+         << setw(19) << duration_blocked_L1.count()
+         << setw(20) << duration_blocked_L2.count()
+         // << setw(20) << duration_blocked_L3.count()
+         << endl;
 
     return 0;
 }
